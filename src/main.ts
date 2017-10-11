6a8fd2b2621317bea54292fa1965be1802068329
Seloris <ddjordjevic@infinitesquare.com>
2017-10-11 22:26:33 +0200

    add rxjs operators

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import './rxjs-operators';
+
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ src/rxjs-operators.ts
@@ -0,0 +1,11 @@
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/debounceTime';
+
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/materialize';
+import 'rxjs/add/operator/dematerialize';
+import 'rxjs/add/operator/mergeMap';
